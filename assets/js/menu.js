303a4e2348dd580a3186da90ba98168888b09b16
Bryan <looez.2015@gmail.com>
2021-04-06 11:49:38 -0600

    creamos la interacción del menú

---
+++ assets/js/menu.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.menu');
+const menu = document.querySelector('.menu-navegacion');
+
+console.log(hamburger);
+console.log(menu);
+
+
+hamburger.addEventListener('click', ()=>{
+  menu.classList.toggle("spread");
+})
+
+window.addEventListener('click', e=>{
+  if(menu.classList.contains('spread') && e.target != menu && e.target != hamburger){
+    menu.classList.toggle("spread");
+  }
+})
